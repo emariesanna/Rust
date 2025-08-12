912480ef38cc2b2d48cb46a82a1f269f13c3b443
emariesanna <ema.sanna98@gmail.com>
2025-08-12 11:19:20 +0200

    es2

--- es2/src/bin/es2_3.rs
+++ es2/src/bin/es2_3.rs
@@ -5,6 +5,7 @@ pub enum Error {
     CannotOpenFile,
     CannotWriteFile,
     ParseError,
+    DirectionError,
     IncorrectArgumentsNumber,
     IncorrectArgumentsValue
 }
@@ -16,6 +17,7 @@ impl Error {
                 Self::CannotOpenFile => "Cannot open file.",
                 Self::CannotWriteFile => "Cannot write file.",
                 Self::ParseError => "Cannot parse arguments.",
+                Self::DirectionError => "Invalid boat direction."
                 Self::IncorrectArgumentsNumber => "Incorrect arguments number.",
                 Self::IncorrectArgumentsValue => "Incorrect arguments values.",
             }
@@ -27,7 +29,7 @@ fn nuova_board(filename: &str, arguments: &str) -> Result<(), Error>{
     let numeri = arguments
         .split(',')
         .map(|s| s.trim().parse::<u8>())
-        .collect::<Result<Vec<_>, _>>()
+        .collect::<Result<Vec<u8>, _>>()
         .map_err(|_| Error::ParseError)?;
         
     if numeri.len() != 4 {
@@ -35,8 +37,8 @@ fn nuova_board(filename: &str, arguments: &str) -> Result<(), Error>{
     }
 
     let mut file = OpenOptions::new()
-        .create(true)      // crea il file se non esiste
-        .append(true)      // aggiunge alla fine del file
+        .create(true)
+        .append(true)
         .open(filename)
         .map_err(|_| Error::CannotOpenFile)?;
 
@@ -53,7 +55,37 @@ fn nuova_board(filename: &str, arguments: &str) -> Result<(), Error>{
 
 fn aggiungi_nave(filename: &str, arguments: &str) -> Result<(), Error>{
     let board = read_to_string(filename).map_err(|_| Error::CannotOpenFile)?;
-    let mut rows: Vec<String> = board.lines().map(|s| s.to_string()).collect();
+    let mut rows: Vec<String> = board.lines().skip(1).map(|s| s.to_string()).collect();
+
+    let argomenti: Vec<String> = arguments.split(",").map(|s| s.to_string()).collect();
+    let direzione = &argomenti[0];
+    if direzione != "V" || direzione != "H" {
+        Err(Error::DirectionError)?
+    }
+    let valori: Vec<u8> = argomenti[1..4]
+        .iter()
+        .map(|s |s.trim().parse::<u8>())
+        .collect::<Result<Vec<u8>, _>>()
+        .map_err(|_| Error::ParseError)?;
+
+    let lunghezza = valori[0];
+    let mut x = valori[1];
+    let mut y = valori[2];
+
+    for _ in 0..lunghezza {
+        let row = &mut rows[(y - 1) as usize];
+        let mut chars: Vec<char> = row.chars().collect();
+        chars[(x - 1) as usize] = 'B';
+        *row = chars.into_iter().collect();
+        if direzione == "V" {
+            y -= 1;
+        } else if direzione == "H" {
+            x -= 1;
+        }
+    }
+
+    Ok(())
+
 }
 
 fn main(){
